7ecc2a14ab87867694a153d5d580bd26aaa545ef
Roshan Prabhakar <roshan@pcp139018pcs.wireless.calpoly.edu>
2017-04-18 15:09:46 -0700

    Display posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const FETCH_POSTS = 'FETCH_POSTS'
+export const CREATE_POST = 'CREATE_POST'
 
 const ROOT_URL = 'http://127.0.0.1:5000'
 
@@ -9,6 +10,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts`, props)
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
--- src/components/PostsView.jsx
+++ src/components/PostsView.jsx
@@ -7,9 +7,36 @@ class PostsView extends Component {
     this.props.fetchPosts()
   }
 
+  renderPosts() {
+    console.log(this.props)
+    return this.props.posts.map((post) => {
+      return (
+        <div className="thumbnail" key={post.id}>
+          <img src="https://www.tesla.com/sites/default/files/blog_images/model-s-photo-gallery-01.jpg" alt="..." />
+            <div className="caption">
+              <h3>{post.title}</h3>
+              <p>{post.description}</p>
+              <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
+            </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    return <h3>List of blog posts</h3>
+    return (
+      <div className="row">
+        <h3>List of posts</h3>
+        <div className="col-sm-6 col-md-4">
+          {this.renderPosts()}
+        </div>
+      </div>
+    )
   }
 }
 
-export default connect(null, { fetchPosts })(PostsView)
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsView)
